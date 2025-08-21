c1bf23b7c9ddb8159951f9770eeec1dd3fd89de5
Jishnu543 <114851670+Jishnu543@users.noreply.github.com>
2025-08-21 17:26:07 +0530

    Update urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views  # import views from devops/devops/views.py
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('demo/', views.demo, name='demo'),  # 👈 add this
     path('admin/', admin.site.urls),
-    path('demo/', include('demo.urls')),  # include the demo app urls
 ]
