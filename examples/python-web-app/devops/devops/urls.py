5d7de60dc61e6539fdfd3ee67d29f3b87703d42e
Jishnu543 <114851670+Jishnu543@users.noreply.github.com>
2025-08-21 14:17:31 +0530

    Update urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from . import views   # make sure views.py is imported
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('demo/', views.index, name='demo'),   # add this
+    path('', views.index, name='index'),      # existing homepage
+    path('demo/', views.index, name='demo'),  # new route for /demo
+    path('admin/', admin.site.urls),
 ]
