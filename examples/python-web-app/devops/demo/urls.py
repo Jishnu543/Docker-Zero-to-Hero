ed69ac70f142caac47139ff0122489b16a4560fc
Jishnu543 <114851670+Jishnu543@users.noreply.github.com>
2025-08-21 13:28:44 +0530

    Update urls.py

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),        # http://...:8000/
+    path('demo/', views.demo, name='demo'),     # http://...:8000/demo
 ]
